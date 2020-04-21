2923e591afeae756cccd6227ddaa156f6fd7ba01
Marina Muratova <frau.muratova@gmail.com>
2020-04-21 11:21:13 +0300

    first commit variables

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -5,23 +5,31 @@
 	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
  */
 
+
+    let algebra = 10;
+    let geography = 10;
+    let physics = 10;
+
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	let averageOfThreeSub = (a + g + p) / 3;
+	return averageOfThreeSub;
 }
+averageMark (algebra, geography, physics);
 
+let myName = 'Marina';
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+	let message = 'Привет, мир! Меня зовут ' + userName;    
+	return message;
+}
+sayHello(myName);
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+function calculateFormula(a, b, c) {
+	let result = a * b + 5 * c + a - 1;
+    return result;
 }
 
-function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
-}
+calculateFormula(x, y, z);
