57c0937177c041ed1ca6ed2817867a48145938cc
Marina Muratova <frau.muratova@gmail.com>
2020-04-24 14:48:04 +0300

    task 1-3 corrected ver 2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -9,37 +9,30 @@ function getResult(a,b,c){
     } else if (discriminant === 0) {
          x1 = -b / (2 * a); 
          x = [x1];
-	    } else {
-		     x = [];
-          }
+	    } 
 
  return x;
 }
 
-
-
-let array = [1,2,3,4,5];
 function getAverageMark(marks){
   let averageMark = 0;
   let sum = 0;
-  if (marks.length > 5 ) {
+  if (marks.length == 0){
+    return 0;
+  } else if (marks.length > 5 ) {
      console.log(`Оценок больше 5, а именно ${marks.length}`);
      marks.splice(5);
-  }
-       for (let i = 0; i < marks.length; i++) {
-        sum += marks[i];
-        averageMark = sum / marks.length;
     } 
-return averageMark;
+      for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+      } 
+ return sum / marks.length;
 }
 
 
-
-let currentDate = new Date();
 function askDrink(name,dateOfBirthday){
- let currentAge = new Date().getFullYear()  - dateOfBirthday;
- let result = (currentAge > 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
- return result;
+ let currentAge = new Date().getFullYear()  - dateOfBirthday.getFullYear();
+   return (currentAge > 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
 }
 
 
