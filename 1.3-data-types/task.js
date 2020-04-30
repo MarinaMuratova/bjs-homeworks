8e582d206f1795158c7bbdda81ccfe4c88fb77c2
Marina Muratova <frau.muratova@gmail.com>
2020-04-30 16:02:21 +0300

    1.3 task corrected after checking

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,43 +1,22 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
 	'use strict'
 
-	if(typeof percent !== "number") {
-		let message = `Параметр "Ставка" содержит неправильное значение${percent}`;
-		console.log(message);
-	} else if (typeof contribution !== "number") {
-		message = `Параметр "Начальный взнос" содержит неправильное значение${contribution}`;
-		console.log(message);
-} else if (typeof amount !== "number") {
-		message = `Параметр "Сумма ипотеки" содержит неправильное значение${amount}`;
-		console.log(message);
-	}
-	
-// 	вторая попытка - сделать массив, по нему пройтись циклом и выявить "не число"
-//let arrParsed = [parseInt(percent), parseInt(contribution), parseInt(amount)];
-//      for (let i = 0; i < arrParsed.length; i++){
-//       if (isNaN(arrParsed[i])) { 
-// 		let  message =  `Параметр содержит неправильное значение`;
-// 		console.log(message); 
-// 	   } else if (date == undefined){
-// 	   	message = `Параметр "Срок ипотеки" содержит неправильное значение ${date}`;
-// 	   	console.log(message); 
-// 	   }
-	
-// }
+const parsedPercent = parseFloat(percent);	
+const parsedСontribution = parseFloat(contribution);
+const parsedAmount = parseFloat(amount);
 
+if(isNaN(percent)) {
+    return (`Параметр "Ставка" содержит неправильное значение${percent}`);
+} 
+if (isNaN(contribution)) {
+    return (`Параметр "Начальный взнос" содержит неправильное значение${contribution}`);
+} 
+if (isNaN(amount)) {
+	return (`Параметр "Сумма ипотеки" содержит неправильное значение${amount}`);
+}
 
-//третья попытка - попробовать перевести "строку" в число, но тогда не понимаю, как в строку сообщения вставить нужное название параметра
-// const parsed = parseInt(percent, contribution, amount);
- //       if (isNaN(parsed)) { 
- // 		let  message = `Параметр ? содержит неправильное значение`;
- // 		console.log(message); 
- // 	   }
-
-  // }
-
-
-	percent = percent / 100;
-	let toGiveBack = amount - contribution;
+percent = percent / 100;
+let toGiveBack = amount - contribution;
 	
 let startDate = new Date().getTime();
 let endDate = date.getTime();
@@ -45,23 +24,17 @@ let dateDifference =  endDate - startDate;
 let remainsDate = new Date(dateDifference);
 let remainsSec = (parseInt(remainsDate / 1000));
 let remainsFullDays = (parseInt(remainsSec / (24 * 60 * 60)));
-let monthsToPay = Math.round(remainsFullDays/30);
+let monthsToPay = Math.round(remainsFullDays / 30);
 
 let percentForMonth =  percent / 12;
-let payForMonth =  toGiveBack*(percentForMonth+percentForMonth/(((1+percentForMonth)**monthsToPay)-1));
-let totalAmount = payForMonth.toFixed(2) * monthsToPay ;
-return totalAmount.toFixed(2)
+let payForMonth =  toGiveBack * (percentForMonth + percentForMonth / (((1 + percentForMonth)**monthsToPay) - 1));
+let totalAmount = payForMonth * monthsToPay ;
+return parseFloat(totalAmount.toFixed(2))
 }
 
 
 
 
 function getGreeting(name) {
-  let greeting = "";
-	if (name == null || name == "") {
-		greeting = "Привет, мир! Меня зовут Аноним";
-	} else {
-         greeting = `Привет, мир! Меня зовут ${name}`;
-	    }
-	return greeting;
+	return (`Привет, мир! Меня зовут ${name || "Аноним"}`);
 }
