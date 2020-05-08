551ce9efc8acb975d575d00ed64d3b62e902418f
Marina Muratova <frau.muratova@gmail.com>
2020-05-08 17:09:50 +0300

    Initial commit

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,32 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+	// let sound = animal.sound;
+    if (animal === undefined){
+    	return null;
+    } 
+    	return animal.sound;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sum = 0;
+     if (marks.length == 0){
+    	return 0;
+     }
+     for (let i = 0; i < marks.length; i++){
+    	sum += Number(marks[i]);
+     }
+    return Math.round(sum/marks.length);
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let currentDate = new Date().getTime();
+
+    if (birthday === null){
+    	return age < 18;
+    }
+
+    let userBirthday = new Date(birthday).getTime();
+    let diff = currentDate - userBirthday;
+    let age = diff * 3.1713814027394087e-11;
+
+    return age > 18;
 }
